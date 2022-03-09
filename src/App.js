933a34d1dfc918fc7dcc7afe3ae9a814817e9bbd
Maks <maks.glukhov@etu.univ-poitiers.fr>
2022-03-09 10:09:51 +0100

    Changed

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello world!
+          Changed!
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
